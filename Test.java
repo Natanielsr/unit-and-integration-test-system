11db0f84bb1c169a6823ff906f557a4901879063
Nataniel Soares Rodrigues <contatonatanielsoares@gmail.com>
2024-02-16 18:53:57 -0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,36 @@
+package com.uaits.tests;
+
+import java.util.List;
+
+public class Test {
+    String name;
+    List<Object> inputs;
+    Object output;
+    Object expectedReturn;
+
+    public Test(String name, List<Object> inputs, Object expectedReturn) {
+        this.name = name;
+        this.inputs = inputs;
+        this.expectedReturn = expectedReturn;
+    }
+
+    public List<Object> getInputs() {
+        return inputs;
+    }
+
+    public Object getOutput() {
+        return output;
+    }
+
+    public Object getExpectedReturn() {
+        return expectedReturn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPassed(){
+        return output.equals(expectedReturn);
+    }
+}
