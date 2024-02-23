a412bcdebf4ca03d359a84319a9014626aa6b1ad
Nataniel Soares Rodrigues <contatonatanielsoares@gmail.com>
2024-02-23 10:52:23 -0300

    Update Console.java

--- utils/Console.java
+++ utils/Console.java
@@ -3,15 +3,33 @@ import com.google.gson.Gson;
 
 public class Console {
 
-    public static void Log(Object obj, ConsoleColor color){
-        System.out.print(color.getColorCode());
-        Log(obj);
-    }
-  
     public static void Log(Object obj){
+        Log(obj, false);
+    }
+
+    public static void LogLn(){
+        Log("", false);
+    }
+
+    public static void LogInLine(Object obj){
+        Log(obj, true);
+    }
+
+    public static void LogColor(Object obj, ConsoleColor color){
+        System.out.print(color.getColorCode());
+        Log(obj, false);
+    }
+
+    public static void Log(Object obj, boolean inLine){
         Gson gson = new Gson();
         String json = gson.toJson(obj);
-        System.out.println(json);
+
+        if(inLine)
+            System.out.print(json);
+        else {
+            System.out.println(json);
+        }
+
         System.out.print(ConsoleColor.DEFAULT.getColorCode());
     }
 }
